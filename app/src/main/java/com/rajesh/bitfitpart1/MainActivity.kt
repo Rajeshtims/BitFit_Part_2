a053c7ed5ecd50a5c10b699700650a41f3eea534
Rajesh Timilsina <rajeshtims@gmail.com>
2024-03-30 22:15:21 -0400

    Updated GIF

--- app/src/main/java/com/rajesh/bitfitpart1/MainActivity.kt
+++ app/src/main/java/com/rajesh/bitfitpart1/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
